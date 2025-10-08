55f439b46abd06ab636f80a8bd9da72c36ba654e
NethuSanjana <Nethudisasekara255@gmail.com>
2025-10-08 16:00:47 +0530

    bell icon

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import FaceRecognition from './pages/FaceRecognition/FaceRecognition';
 import Profile from './pages/Profile/Profile';
 import Payment from './pages/payment/payment';
 import Bill from './pages/bill/bill';
+import NotificationPage from './pages/Home/notification';
 
 import Team from './pages/Team/Team';
 import Admin from './AdminPanel/Admin';
@@ -40,6 +41,7 @@ function App() {
         <Route path="/payment" element={<Payment />} />
         <Route path="/bill" element={<Bill />} />
         <Route path="/face-recognition" element={<FaceRecognition />} />
+  <Route path="/notifications" element={<NotificationPage />} />
         <Route path="/team" element={<Team />} />
         <Route path="/Team" element={<Team />} />
         <Route path="/admin" element={<Admin />} />
